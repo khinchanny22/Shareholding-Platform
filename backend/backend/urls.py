4f068f519c1dd8a15042ff7fe0ad2338ee083546
khinchanny22 <112239828+khinchanny22@users.noreply.github.com>
2022-08-28 12:20:48 +0700

    add syste

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('login/', include('login.urls')),
+    path('backend/', include('login.urls')),
     path('', include('frontend.urls')),
     path('admin/', admin.site.urls),
 ]
