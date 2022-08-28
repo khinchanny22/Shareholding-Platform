062d70c63236cd50fb5df2bcf31a28b147e4c4de
khinchanny22 <112239828+khinchanny22@users.noreply.github.com>
2022-08-28 14:01:38 +0700

    customer layout

--- backend/login/models.py
+++ backend/login/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class Users(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+
+    def __int__(self):
+        return self.username
--- backend/login/views.py
+++ backend/login/views.py
@@ -1,10 +1,27 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, request
 from django.shortcuts import render
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from .models import Users
 
 
 # Create your views here.
+
+
 def Login(request):
-    return render(request, 'backend/dashboard.html')
+    if request.method == "GET":
+        return render(request, 'login/index.html')
+    username = request.POST.get('user')
+    password = request.POST.get('pwd')
+
+    user = Users.objects.get(id=1)
+    user_name = user.username
+    pass_word = user.password
+
+    if username == user_name and password == pass_word:
+        return HttpResponse('Hello')
+    return render(request, "login/index.html", {'error_msg': 'hello'})
 
 
 def Backend(request):
