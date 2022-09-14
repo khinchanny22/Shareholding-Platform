b922f85ada66571d9ddd5268aea93432a3876ebc
khinchanny22 <112239828+khinchanny22@users.noreply.github.com>
2022-09-14 21:53:20 +0700

    add sth

--- backend/buyer/views.py
+++ backend/buyer/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse
+from .forms import *
 
 # Create your views here.
+
+
 def SellersIndex(request):
-    return render(request, 'backend/buyers/seller.html')
+    form = SellerForm()
+    content = {
+        'form':form
+    }
+    return render(request, 'backend/buyers/seller.html',content)
--- backend/customer/models.py
+++ backend/customer/models.py
@@ -8,6 +8,14 @@ class Customer(models.Model):
     customer_contact = models.CharField(max_length=15, unique=True)
     customer_email = models.EmailField(unique=True)
     Customer_origin = models.CharField(max_length=20)
+    customer_status = (
+        ("SELECT", "SELECT"),
+        ("Seller", "Seller"),
+        ("Buyer", "Buyer"),
+    )
+    customer_status = models.CharField(max_length=20,
+                               choices=customer_status,
+                               default="SELECT")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
