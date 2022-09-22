875bce8c41fde79ab5807ab8da7c71e6b6b8d0b3
khinchanny22 <khinchanny2022@gmail.com>
2022-09-23 00:48:34 +0700

    update login

--- backend/frontend/views.py
+++ backend/frontend/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
--- backend/login/urls.py
+++ backend/login/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Login, name='Login'),
-    path('', views.signout, name='signout'),
+    path("register", views.register_request, name="register"),
+    path('', views.login_request, name="login_request"),
+    path("logout_request", views.logout_request, name="logout_request"),
 ]
